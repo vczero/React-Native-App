4d1f27c24ce91614b67f1815b7ecb9fba00c056d
lh_wang <lh_wang@Ctrip.com>
2015-07-14 16:07:54 +0800

    black statuBar

--- address_book/index.ios.js
+++ address_book/index.ios.js
@@ -5,7 +5,7 @@ var Home = require('./views/home');
 var About = require('./views/about');
 var Manager = require('./views/manager');
 var Message = require('./views/message');
-var Login = require('./views/login');
+var Util = require('./views/util');
 
 var {
   StyleSheet,
@@ -14,7 +14,10 @@ var {
   Text,
   NavigatorIOS,
   AppRegistry,
+  Image,
+  TextInput,
   StatusBarIOS,
+  TouchableHighlight,
   } = React;
 
 StatusBarIOS.setStyle('light-content');
@@ -28,14 +31,13 @@ var Address =  React.createClass({
     return {
       selectedTab: 'home',
       showIndex: {
-        height:0
+        height:0,
+        opacity:0
       },
       showLogin:{
-        flex:1
+        flex:1,
+        opacity:1
       },
-      showTabBar:{
-        opacity:0
-      }
     };
   },
 
@@ -53,22 +55,53 @@ var Address =  React.createClass({
       tintColor="#fff"
       translucent={false}
       initialRoute={{
-              component: component,
-              title: title,
-            }}
+          component: component,
+          title: title,
+        }}
       />;
+  },
+
+  _login: function(){
+    this.setState({
+      showLogin: {
+        //opacity:0,
+        height:18,
+        backgroundColor: '#000',
+      },
+      showIndex:{
+        flex:1,
+        opacity:1
+      }
+    });
   },
 
   render: function(){
     return(
       <View style={{flex:1}}>
 
-        <View style={this.state.showLogin}>
-          <Login></Login>
+        <View style={[this.state.showLogin]}>
+          <View style={styles.container}>
+            <View>
+              <Image style={styles.logo} source={require('image!logo')}></Image>
+            </View>
+
+            <View style={styles.inputRow}>
+              <Text>邮箱</Text><TextInput style={styles.input} placeholder="请输入邮箱"/>
+            </View>
+            <View style={styles.inputRow}>
+              <Text>密码</Text><TextInput style={styles.input} placeholder="请输入密码" password={true}/>
+            </View>
+
+            <View>
+              <TouchableHighlight underlayColor="#fff" style={styles.btn} onPress={this._login}>
+                <Text style={{color:'#fff'}}>登录</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
         </View>
 
         <View style={this.state.showIndex}>
-          <TabBarIOS barTintColor="#FFF" style={this.state.showTabBar}>
+          <TabBarIOS barTintColor="#FFF">
             <TabBarIOS.Item
               icon={require('image!phone_s')}
               title="首页"
@@ -111,6 +144,42 @@ var Address =  React.createClass({
     );
   }
 
+});
+
+var styles = StyleSheet.create({
+  container:{
+    marginTop:50,
+    alignItems:'center',
+  },
+  logo:{
+    width:100,
+    height:100,
+    resizeMode: Image.resizeMode.contain
+  },
+  inputRow:{
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent: 'center',
+    marginBottom:10,
+  },
+  input:{
+    marginLeft:10,
+    width:220,
+    borderWidth:Util.pixel,
+    height:35,
+    paddingLeft:8,
+    borderRadius:5,
+    borderColor:'#ccc'
+  },
+  btn:{
+    marginTop:10,
+    width:80,
+    height:35,
+    backgroundColor:'#3BC1FF',
+    justifyContent:'center',
+    alignItems:'center',
+    borderRadius: 4,
+  }
 });
 
 
